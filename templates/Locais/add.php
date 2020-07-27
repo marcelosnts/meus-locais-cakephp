7d75fc4ecae4c9b62a8c962a03ac126f018651d9
Marcelo <santosmarcelo1995@live.com>
2020-07-27 19:30:09 -0300

    CEP Ajax request and form value auto update

--- templates/Locais/add.php
+++ templates/Locais/add.php
@@ -16,3 +16,25 @@
     echo $this->Form->button(__('Salvar local'));
     echo $this->Form->end();
 ?>
+
+<?= $this->Html->script('jquery') ?>
+
+<script>
+    $('#cep').change(() => {
+        var cep = $('#cep').val();
+
+        if (cep.length === 8){
+            var url = `https://viacep.com.br/ws/${cep}/json/`;
+
+            $.getJSON(url)
+            .done(response => {
+                $('#logradouro').val(response.logradouro);
+                $('#complemento').val(response.complemento);
+                $('#bairro').val(response.bairro);
+                $('#uf').val(response.uf);
+                $('#cidade').val(response.localidade);
+                console.log(response);
+            })
+        }
+    })
+</script>
