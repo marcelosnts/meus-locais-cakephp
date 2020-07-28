a80eb47633b4b5c08309ef79d7065776a1c14397
Marcelo <santosmarcelo1995@live.com>
2020-07-28 10:52:49 -0300

    Define validation rules and messages

--- src/Model/Table/LocaisTable.php
+++ src/Model/Table/LocaisTable.php
@@ -5,21 +5,18 @@
     use Cake\Validation\Validator;
 
     class LocaisTable extends Table {
-        public function initialize(array $config) : void {
-
-        }
+        public function initialize(array $config) : void {}
 
         public function validationDefault(Validator $validator) : Validator {
-            $validator
-                ->notEmptyString('nome')
-                ->notEmptyString('cep')
-                ->notEmptyString('logradouro')
-                ->notEmptyString('complemento')
-                ->notEmptyString('numero')
-                ->notEmptyString('bairro')
-                ->notEmptyString('uf')
-                ->notEmptyString('cidade')
-                ->notEmptyString('data');
+            $validator->add('nome', 'required', ['rule' => 'notBlank', 'message' => 'O nome é obrigatório']);
+            $validator->add('cep', 'required', ['rule' => 'notBlank', 'message' => 'Favor informar o CEP para a consulta']);
+            $validator->add('logradouro', 'required', ['rule' => 'notBlank', 'message' => 'O logradouro é obrigatório']);
+            $validator->add('complemento', 'required', ['rule' => 'notBlank', 'message' => 'O complemento é obrigatório']);
+            $validator->add('numero', 'required', ['rule' => 'notBlank', 'message' => 'O numero é obrigatório']);
+            $validator->add('bairro', 'required', ['rule' => 'notBlank', 'message' => 'O bairro é obrigatório']);
+            $validator->add('uf', 'required', ['rule' => 'notBlank', 'message' => 'A uf é obrigatória']);
+            $validator->add('cidade', 'required', ['rule' => 'notBlank', 'message' => 'A cidade é obrigatória']);
+            $validator->add('data', 'required', ['rule' => 'notBlank', 'message' => 'A data é obrigatória']);
 
             return $validator;
         }
